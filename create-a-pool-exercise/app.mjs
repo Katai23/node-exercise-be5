27e0e03bdef3082be14c227e5184e58c4abd40e0
Prapaporn Srijak <srijak.tay@gmail.com>
2026-07-05 10:46:39 +0700

    creating-records-answer

--- create-a-pool-exercise/app.mjs
+++ create-a-pool-exercise/app.mjs
@@ -7,11 +7,16 @@ const port = 4000;
 app.use(express.json());
 
 app.get("/movies", async (req, res) => {
+
+
 	const result = await pool.query("select * from movies");
 
+	//Respond object and put all data inside of it in JSON style
 	return res.json({
 		data: result.rows,
 	});
+
+
 });
 
 app.listen(4000, () => {
--- creating-records-exercise/app.mjs
+++ creating-records-exercise/app.mjs
@@ -8,6 +8,25 @@ app.use(express.json());
 
 // 📍 **** สร้าง API เพื่อใช้ในการเพิ่มข้อมูลหนังเรื่องใหม่ไปที่ Database ตรงนี้ ****
 
+app.post("/movies", async (req, res) => {
+
+	const title =	req.body.title;
+	const description = req.body.description;
+	const genres = req.body.genres;
+	const year = req.body.year;
+	const poster = req.body.poster;
+	const rating = req.body.rating;
+
+	const result = await pool.query(`
+		INSERT INTO movies (title, description, genres, year, poster, rating)
+		VALUES ($1, $2, $3, $4, $5, $6)`,
+		[title, description, genres, year, poster, rating]);
+	
+
+	return res.json({message: "Movie has been created. "});
+
+});
+
 app.listen(port, () => {
 	console.log(`🚀 Server is running at ${port}`);
 });
